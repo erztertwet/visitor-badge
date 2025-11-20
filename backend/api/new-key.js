a3aa96e4f89ad8a04a3cb3d57d6238d066d6ae3b
stefanopaolonii <stefanopaoloni.work@gmail.com>
2025-11-20 18:41:09 +0100

    added backend

---
+++ backend/api/new-key.js
@@ -0,0 +1,37 @@
+import redis from "../src/redis.js";
+import { randomUUID } from "crypto";
+
+async function generateNewKey() {
+    let key;
+
+    while (true) {
+        key = randomUUID();
+        const exists = await redis.exists(`counter:${key}`);
+
+        if (!exists) {
+            break;
+        }
+    }
+
+    return key;
+}
+
+export default async function handler(req, res) {
+    try {
+    const key = await generateNewKey();
+
+    await redis.set(`counter:${key}`, 0);
+
+    const baseUrl = process.env.BASE_URL ;
+    const badgeUrl = `${baseUrl}/api/view/${key}`;
+    const markdown = `![Visits](https://img.shields.io/endpoint?url=${badgeUrl})`;
+
+    res.status(200).json({
+        key,
+        badgeUrl,
+        markdown,
+    });
+    } catch (error) {
+        res.status(500).json({ error: "Internal Server Error" });
+    }
+}

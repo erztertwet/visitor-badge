edf23b2bdd566a05004277504173bb6ef8c88161
stefanopaolonii <stefanopaoloni.work@gmail.com>
2025-11-20 22:44:09 +0100

    redis update

--- backend/src/redis.js
+++ backend/src/redis.js
@@ -1,10 +1,18 @@
 import Redis from 'ioredis';
+import { lazy } from 'react';
 
 const redis = new Redis({
     host: process.env.REDIS_HOST,
     port: process.env.REDIS_PORT,
     username: "default",
     password: process.env.REDIS_PASSWORD,
+    maxRetriesPerRequest: 1,
+    enableOfflineQueue: false,
+    connectTimeout: 10000,
+    retryStrategy: (times) => {
+        const delay = Math.min(times * 50, 2000);
+        return delay;
+    }
 });
 
 export default redis;

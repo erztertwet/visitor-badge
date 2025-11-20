8c436f37598020ba26d9858fc6a65ddd2220214e
stefanopaolonii <stefanopaoloni.work@gmail.com>
2025-11-20 21:18:32 +0100

    redis configuration update

--- backend/src/redis.js
+++ backend/src/redis.js
@@ -1,9 +1,7 @@
 import Redis from 'ioredis';
 
 const redis = new Redis({
-    host: process.env.REDIS_HOST,
-    port: process.env.REDIS_PORT,
-    password: process.env.REDIS_PASSWORD
+    process.env.REDIS_URL,
 });
 
 export default redis;
